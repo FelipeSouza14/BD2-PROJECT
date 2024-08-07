db7da88243369e7e03ec65aedece9639f06a9e36
FelipeSouza14 <felipe.dev.2003@gmail.com>
2024-08-07 09:23:13 -0300

    fechando a conexão ODBC #1

--- tarefas/orm/script-odbc.py
+++ tarefas/orm/script-odbc.py
@@ -13,24 +13,25 @@ cursor = conn.cursor()
 
 
 # Inserir uma atividade em um projeto
-# inserir_nova_atividade = """
-# INSERT INTO atividades (nome, descricao, data_inicio, data_fim, projeto_id) 
-# VALUES (%s, %s, %s, %s, %s);
-# """
-# dados_nova_atividade = ('Atividade A', 'Descrição da Atividade A', '2024-08-06', '2024-08-07', 1)
-# cursor.execute(inserir_nova_atividade, dados_nova_atividade)
+inserir_nova_atividade = """
+INSERT INTO atividades (nome, descricao, data_inicio, data_fim, projeto_id) 
+VALUES (%s, %s, %s, %s, %s);
+"""
+dados_nova_atividade = ('Atividade A', 'Descrição da Atividade A', '2024-08-06', '2024-08-07', 1)
+cursor.execute(inserir_nova_atividade, dados_nova_atividade)
 
-# conn.commit()
+conn.commit()
 
-# atualizar_lider = """
-#     UPDATE projetos 
-#     SET lider = %s 
-#     WHERE id = %s;
-#     """
-# novo_lider = ('Benicio', 1)
-# cursor.execute(atualizar_lider, novo_lider)
+# Atualizando o lider do projeto
+atualizar_lider = """
+    UPDATE projetos 
+    SET lider = %s 
+    WHERE id = %s;
+    """
+novo_lider = ('Benicio', 1)
+cursor.execute(atualizar_lider, novo_lider)
 
-# conn.commit()
+conn.commit()
 
 cursor.execute("SELECT * FROM projetos ORDER BY id")
 
@@ -44,11 +45,11 @@ rows = cursor.fetchall()
 for row in rows:
     print(row)
 
-# # Fechar a conexão
-# cursor.close()
-# conn.commit()
-# conn.close()
-# print("Conexão fechada.")
+# Fechar a conexão
+cursor.close()
+conn.commit()
+conn.close()
+print("Conexão fechada.")
 
 
 
