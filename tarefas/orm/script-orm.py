593e610d6083714391d1d7c1645c55c06f66ab13
FelipeSouza14 <felipe.dev.2003@gmail.com>
2024-08-07 09:28:27 -0300

    utilizando o framework ORM sqlalchemy #1

---
+++ tarefas/orm/script-orm.py
@@ -0,0 +1,4 @@
+from sqlalchemy import create_engine, Column, Integer, String, Text, Date, ForeignKey
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
