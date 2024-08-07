5d1e96a69b567c669b0e0b959573a0314a5b885a
FelipeSouza14 <felipe.dev.2003@gmail.com>
2024-08-07 09:07:18 -0300

    nova atividade inserida em um projeto #1

--- tarefas/orm/script-odbc.py
+++ tarefas/orm/script-odbc.py
@@ -3,12 +3,30 @@ import psycopg2
 
 conn = psycopg2.connect(
     dbname='AtividadesBD',
-    user='FelipeSouza14',        # substitua pelo seu usuário
-    password='sua_senha',      # substitua pela sua senha
-    host='localhost',          # ou o IP do contêiner, se for diferente
+    user='FelipeSouza14',        
+    password='felipebd1234',      
+    host='localhost',          
     port='5432'
 )
 
-cur = conn.cursor()
+cursor = conn.cursor()
+
+
+# Inserir uma atividade em um projeto
+inserir_nova_atividade = """
+INSERT INTO atividades (nome, descricao, data_inicio, data_fim, projeto_id) 
+VALUES (%s, %s, %s, %s, %s);
+"""
+dados_nova_atividade = ('Atividade A', 'Descrição da Atividade A', '2024-08-06', '2024-08-07', 1)
+cursor.execute(inserir_nova_atividade, dados_nova_atividade)
+
+conn.commit()
+
+# # Fechar a conexão
+# cursor.close()
+# conn.commit()
+# conn.close()
+# print("Conexão fechada.")
+
 
 
